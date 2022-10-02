c19f315031a1392a78d41deb4abb23c06e6a6769
Dendi <dendiaa@students.amikom.ac.id>
2022-10-02 22:18:03 +0700

    feat: 1735 A Working Week

---
+++ codeforces/WorkingWeek.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(){
+    int tt, c;
+    scanf("%d", &tt);
+
+    while(tt--){
+        scanf("%d", &c);
+        cout<<(c/3) - 2<<endl;
+    }
+
+    return 0;
+}
