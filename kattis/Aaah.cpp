0430d4308348ec5f45b44bc738d1b0ce915c57a3
Dendi <dendiaa@students.amikom.ac.id>
2022-10-18 22:23:17 +0700

    Aaah!

---
+++ kattis/Aaah.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main(){
+    string a,b;
+    cin>>a>>b;
+    if (a.size() < b.size()){
+        cout<<"no"<<endl;
+    } else{
+        cout<<"go"<<endl;
+    }
+    
+    return 0;
+}
