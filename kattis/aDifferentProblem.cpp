ee9e6c862d03240c0d73b7f6f4655a31e1797672
Dendi <dendiaa@students.amikom.ac.id>
2022-10-24 21:52:59 +0700

    feat: A Different Problem

---
+++ kattis/aDifferentProblem.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll a, b;
+
+    while(cin>>a){
+        cin>>b;
+        cout << abs(a -b) <<endl;
+    }
+
+    return 0;
+}
